3e0e68af838027999c211eaec0939d2a2b5e079d
Yarden Bar <yarden@seekingalpha.com>
2016-01-11 19:41:03 +0200

    Set service provider to systemd on Debian/Ubuntu

--- manifests/services.pp
+++ manifests/services.pp
@@ -1,7 +1,13 @@
 class circus::services {
+  $service_provider = $::operatingsystem ? {
+    /(Debian|Ubuntu)/ => 'systemd',
+    default           => 'init',
+  }
+
   service { 'circus':
     ensure     => 'running',
     enable     => true,
     hasrestart => true,
+    provider   => $service_provider,
   }
 }
